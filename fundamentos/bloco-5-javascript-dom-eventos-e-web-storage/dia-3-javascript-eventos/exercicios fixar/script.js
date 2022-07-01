5889b688f8cddaf07019389d54badc2eb6294af5
João Victor Schiavon <schiavonj@yahoo.com.br>
2022-07-01 01:51:25 -0300

    exercise 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicios fixar/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicios fixar/script.js
@@ -14,6 +14,7 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+  // exercício 1
 
   let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
@@ -44,3 +45,10 @@ for (let i in decemberDaysList) {
 
 	}
 }
+
+// exercício 2
+
+let element1 = document.getElementsByClassName('buttons-container')[0];
+element1.appendChild(addElement('button', 'Feriados', '')).setAttribute('id', 'btn-holiday');
+
+
