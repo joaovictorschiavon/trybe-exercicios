d2312958c58660b40c63dc94d0662baf20101b78
João Victor Schiavon <schiavonj@yahoo.com.br>
2022-07-04 00:03:10 -0300

    feat: localstorage set and get

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -1,3 +1,18 @@
+window.addEventListener("load", setCssValuesOnLoad )
+
+function setCssValuesOnLoad() {
+	let bkg = localStorage.getItem("storagedBackgroundColor");
+	pElement.style.backgroundColor = bkg;
+	let tcolor = localStorage.getItem("storagedTextColor");
+	pElement.style.color = tcolor;
+	let fsize = localStorage.getItem("storagedFontSize");
+	pElement.style.fontSize = fsize;
+	let lheight = localStorage.getItem("storagedLineHeight");
+	pElement.style.lineHeight = lheight;
+	let fofamily = localStorage.getItem("storagedFontFamily");
+	pElement.style.fontFamily = fofamily;
+}
+
 let submitButton = document.getElementById("btn-submit");
 submitButton.addEventListener("click", getPreferences);
 let pElement = document.getElementsByTagName("p")[0];
@@ -6,7 +21,7 @@ function getPreferences() {
 	let nbgColor = document.getElementById("bgroundColor").value;
 	let ntextColor = document.getElementById("textColor").value;
 	let nfontSize = document.getElementById("fontSize").value;
-	let nlineHeigth = document.getElementById("lineHeigth").value;
+	let nlineHeight = document.getElementById("lineHeight").value;
 	let nfontFamily = document.getElementById("fontFamily").value;
 
 	if (nbgColor.length > 0) {
@@ -19,9 +34,23 @@ function getPreferences() {
 		pElement.style.fontSize = nfontSize;
 	}
 	if (nlineHeigth.length > 0) {
-		pElement.style.lineHeight = nlineHeigth;
+		pElement.style.lineHeight = nlineHeight;
 	}
 	if (nfontFamily.length > 0) {
 		pElement.style.fontFamily = nfontFamily;
 	}
+	saveCssProperties();
+}
+
+function saveCssProperties() {
+	localStorage.setItem("storagedBackgroundColor", 
+	pElement.style.backgroundColor);
+	localStorage.setItem("storagedTextColor",
+	pElement.style.color);
+	localStorage.setItem("storagedFontSize", 
+	pElement.style.fontSize);
+	localStorage.setItem("storagedLineHeight", 
+	pElement.style.lineHeight);
+	localStorage.setItem("storagedFontFamily", 
+	pElement.style.fontFamily);
 }
