159fc622bec79a36b0b43c49c1cf4cc64cbbdb57
João Victor Schiavon <schiavonj@yahoo.com.br>
2022-07-03 21:38:56 -0300

    feat: formulário de captura e mudança de css properties criado.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -1,6 +1,27 @@
 let submitButton = document.getElementById("btn-submit");
 submitButton.addEventListener("click", getPreferences);
+let pElement = document.getElementsByTagName("p")[0];
 
 function getPreferences() {
-	
+	let nbgColor = document.getElementById("bgroundColor").value;
+	let ntextColor = document.getElementById("textColor").value;
+	let nfontSize = document.getElementById("fontSize").value;
+	let nlineHeigth = document.getElementById("lineHeigth").value;
+	let nfontFamily = document.getElementById("fontFamily").value;
+
+	if (nbgColor.length > 0) {
+		pElement.style.backgroundColor = nbgColor;
+	}
+	if (ntextColor.length > 0) {
+		pElement.style.color = ntextColor;
+	}
+	if (nfontSize.length > 0) {
+		pElement.style.fontSize = nfontSize;
+	}
+	if (nlineHeigth.length > 0) {
+		pElement.style.lineHeight = nlineHeigth;
+	}
+	if (nfontFamily.length > 0) {
+		pElement.style.fontFamily = nfontFamily;
+	}
 }
