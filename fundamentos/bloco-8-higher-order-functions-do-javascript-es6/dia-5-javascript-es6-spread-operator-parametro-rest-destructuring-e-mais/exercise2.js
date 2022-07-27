c11fd2bf38133a809f1e51937a0abf528da7f1b4
João Victor Schiavon <schiavonj@yahoo.com.br>
2022-07-27 19:50:24 -0300

    done exercises

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise2.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise2.js
@@ -1,1 +1,10 @@
-// 2. 
+// 2. Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
+
+function sumVariable(...args) {
+	let size = args.length;
+	let sum = 0;
+	for (let i = 0; i < size; i += 0) {
+		sum += args[i];
+	}
+	return sum;
+}
